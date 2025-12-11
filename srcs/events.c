661790099a246b0da07bacfecf31657f4c2984ca
FurkanTheNazim <moussafurkan0@gmail.com>
2025-12-12 01:45:34 +0300

    fix: zoom now follows mouse cursor position

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: mahmmous <mahmmous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 07:41:46 by mahmmous          #+#    #+#             */
-/*   Updated: 2025/12/10 07:46:25 by mahmmous         ###   ########.fr       */
+/*   Updated: 2025/12/12 01:42:02 by mahmmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,23 @@ int	key_handler(int keysym, t_fractal *fractal)
 
 int	mouse_handler(int button, int x, int y, t_fractal *fractal)
 {
-	(void)x;
-	(void)y;
+	double	mouse_re;
+	double	mouse_im;
+	double	zoom_factor;
+
+	mouse_re = (map(x, (t_map){-2, 2, 0, WIDTH}) * fractal->zoom)
+		+ fractal->shift_x;
+	mouse_im = (map(y, (t_map){2, -2, 0, HEIGHT}) * fractal->zoom)
+		+ fractal->shift_y;
 	if (button == 4)
-		fractal->zoom *= 0.95;
+		zoom_factor = 0.95;
 	else if (button == 5)
-		fractal->zoom *= 1.05;
+		zoom_factor = 1.05;
+	else
+		return (0);
+	fractal->shift_x = mouse_re - (mouse_re - fractal->shift_x) * zoom_factor;
+	fractal->shift_y = mouse_im - (mouse_im - fractal->shift_y) * zoom_factor;
+	fractal->zoom *= zoom_factor;
 	fractal_render(fractal);
 	return (0);
 }
